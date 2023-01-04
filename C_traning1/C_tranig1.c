4934f3ba101fb4e1e473ad03fda1f8d69d0aeec4
LeeSeongKyu <80861684+LeeSeongKyu-920220@users.noreply.github.com>
2023-01-04 18:37:37 +0900

    1193 풀이

--- C_traning1/C_tranig1.c
+++ C_traning1/C_tranig1.c
@@ -1,55 +1,6 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdbool.h>
 
-bool IsEven(int num)
-{
-    if (num % 2 == 0)
-        return true;
-    else
-        return false;
-}
+/*
+				=========== 개인 공부를 위한 백준 C로풀기
+				========================================
 
-void SearchN(const int n, int* head, int* denominator)
-{
-    int count = 0;
-    int sum = 0;
-    int go = 0;
-
-    while (n > sum)
-    {
-        count++;
-        sum += count;
-    }
-
-    sum -= count;
-    go = n - sum;
-
-    if (IsEven(count))
-    {
-        *head = go;
-        *denominator = (count - go) + 1;
-    }
-    else if (!IsEven(count))
-    {
-        *head = (count - go) + 1;
-        *denominator = go;
-    }
-
-}
-int main(void)
-{
-    int inputNum = 0;
-    scanf("%d", &inputNum);
-
-    if (inputNum < 1 || inputNum > 10000000)
-        return -1;
-
-    int head = 0;
-    int denominator = 0;
-
-    SearchN(inputNum, &head, &denominator);
-    printf("%d/%d", head, denominator);
-
-    return 0;
-}
+*/
